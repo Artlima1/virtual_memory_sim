2847558b153d5a934f5d96654ebb726b1a6dff1c
Artlima1 <arthurdeolima@gmail.com>
2023-06-02 18:02:43 -0300

    Main: Getting params, init vm and read sim file

--- main.c
+++ main.c
@@ -1,10 +1,51 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "virtual_memory.h"
+
+int get_subs_alg_type(char * arg){
+    if(strcmp(arg, "lru")==0){
+        return SUBS_ALG_LRU;
+    }
+
+    if(strcmp(arg, "2a")==0){
+        return SUBS_ALG_2ND;
+    }
+
+    if(strcmp(arg, "fifo")==0){
+        return SUBS_ALG_FIFO;
+    }
+
+    if(strcmp(arg, "random")==0){
+        return SUBS_ALG_RAND;
+    }
+}
 
 int main(int argc, char **argv){
 
     printf("Executando o simulador...\n");
+    printf("Arquivo de entrada: %s\n", argv[1]);
+
+    vm_init(get_subs_alg_type(argv[2]), atoi(argv[4]), atoi(argv[3]));
+
+    FILE * sim_file;
+    char file_name[50] = "./simulations/";
+    strcat(file_name, argv[2]);
+    sim_file = fopen(file_name, "r");
+
+    unsigned addr;
+    char rw;
+    while (fscanf(sim_file,"%x %c",&addr,&rw) == 2) {
+        if(rw == 'W'){
+            vm_write(addr);
+        }
+        if(rw == 'R'){
+            vm_read(addr);
+        }
+    }
+
+    vm_print_report();
 
     return 0;
 }
--- virtual_memory.c
+++ virtual_memory.c
@@ -1,17 +1,43 @@
 #include "virtual_memory.h"
 
+#define PRINT_DEBUG
+
+#ifdef PRINT_DEBUG
+#include <stdio.h>
+#endif
+
 void vm_init(int subs_alg_type, int total_memory_size, int page_size){
+
+    #ifdef PRINT_DEBUG
+    printf("Initial params: %d %d %d\n", subs_alg_type, total_memory_size, page_size);
+    #endif
+
     return;
 }
 
 void vm_write(int addr){
+
+    #ifdef PRINT_DEBUG
+    printf("Writing at %d\n", addr);
+    #endif
+
     return;
 }
 
 void vm_read(int addr){
+
+    #ifdef PRINT_DEBUG
+    printf("Reading from %d\n", addr);
+    #endif
+
     return;
 }
 
 void vm_print_report(){
+
+    #ifdef PRINT_DEBUG
+    printf("Printing report...\n");
+    #endif
+
     return;
 }
