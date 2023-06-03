a8ad323363490113317bc58ece6d9f9ea973e741
Artlima1 <arthurdeolima@gmail.com>
2023-06-03 17:41:52 -0300

    Setting memory info

--- virtual_memory.c
+++ virtual_memory.c
@@ -6,11 +6,41 @@
 #include <stdio.h>
 #endif
 
+#define PAGE_ADDR(addr) addr>>mem_info.s
+
+typedef struct {
+    int page_size;
+    int mem_size;
+
+    int s;
+
+    int dirty_pages;
+    int page_faults;
+    int tot_mem_reqs;
+
+} memory_info_t;
+
+static memory_info_t mem_info;
+
+static int calc_s(int page_size){
+    int tmp = page_size;
+    int s = 0;
+    while (tmp>1) {
+        tmp = tmp>>1;
+        s++;
+    }
+    return s;
+}
+
 void vm_init(int subs_alg_type, int total_memory_size, int page_size){
 
     #ifdef PRINT_DEBUG
     printf("Initial params: %d %d %d\n", subs_alg_type, total_memory_size, page_size);
     #endif
+
+    mem_info.mem_size = total_memory_size;
+    mem_info.page_size = page_size;
+    mem_info.s = calc_s(page_size);
 
     return;
 }
